5f0b0eca10d710d614062fdd0d368d7a306939fa
develop-build <deepak7business@gmail.com>
2023-10-16 02:08:05 +0530

    console deployed contract

---
+++ contracts/Inbox.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+
+    function setMessage(string memory newMessage) public  {
+        message = newMessage;
+    }
+}
